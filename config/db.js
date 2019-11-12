e00b962006a7ba2ca2f529e567cc0cf4b4e71ffd
mcast045 <54549449+mcast045@users.noreply.github.com>
2019-11-12 11:52:07 -0800

    mLab additions

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@
 const mongoose      = require('mongoose');
 mongoose.Promise    = global.Promise;
 
-mongoose.connect(process.env.DB_URL, { useNewUrlParser: true });
+mongoose.connect(process.env.DB_URL || "mongodb://user:Ruber1@ds061601.mlab.com:61601/heroku_4dvsgwzl", { useNewUrlParser: true });
 mongoose.connection.once('open', ()=> console.log(`Connected to Mongo at ${process.env.DB_URL}`));
 
 module.exports = mongoose;
--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express       = require('express');
+const path          = require('path');
 const cors          = require('cors');
 const usersRoutes   = require('./routes/users');
 const ridesRoutes   = require('./routes/rides');
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 // require .env and db.js
 require('dotenv').config();
@@ -20,6 +21,14 @@ app.use(cors());
 app.use('/api/users', usersRoutes);
 app.use('/api/rides', ridesRoutes);
 
+//Serve up static assets if in production (for heroku)
+if(process.env.NODE_ENV === 'production')
+{
+    app.use(express.static("client/build"));
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname + 'client', 'build', 'index.html'));
+    })
+}
 
 app.listen(port, () => console.log(`Server is running on port: ${port}`));
 
