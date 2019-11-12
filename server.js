4772bcad0af060056b868719d9762ea2729f205d
mcast045 <54549449+mcast045@users.noreply.github.com>
2019-11-12 11:55:06 -0800

    Build change

--- server.js
+++ server.js
@@ -21,14 +21,6 @@ app.use(cors());
 app.use('/api/users', usersRoutes);
 app.use('/api/rides', ridesRoutes);
 
-//Serve up static assets if in production (for heroku)
-if(process.env.NODE_ENV === 'production')
-{
-    app.use(express.static("client/build"));
-    app.get('*', (req,res) => {
-        res.sendFile(path.resolve(__dirname + 'client', 'build', 'index.html'));
-    })
-}
 
 app.listen(port, () => console.log(`Server is running on port: ${port}`));
 
